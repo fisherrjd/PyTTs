84bf9139386b4b73cbbba76fc5ff589e7c082591
fisherrjd <fisherrjd@gmail.com>
2023-04-27 23:53:40 +0000

    add google api credentials

--- src/mic.py
+++ src/mic.py
@@ -5,3 +5,40 @@ r = sr.Recognizer()
 with sr.Microphone() as source:
     print("Say something!")
     audio = r.listen(source)
+
+    # recognize speech using google speech recognition
+try:
+    # for testing purposes, we're just using the default API key
+    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+    # instead of `r.recognize_google(audio)`
+    print(
+        "Google Speech Recognition thinks you said "
+        + r.recognize_google(
+            audio,
+            key="657215957578-ikc0765fs4geri7tj55pqgrgtbrndr7p.apps.googleusercontent.com",
+        )
+    )
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print(
+        "Could not request results from Google Speech Recognition service; {0}".format(
+            e
+        )
+    )
+
+# recognize speech using Google Cloud Speech
+GOOGLE_CLOUD_SPEECH_CREDENTIALS = (
+    r"""INSERT THE CONTENTS OF THE GOOGLE CLOUD SPEECH JSON CREDENTIALS FILE HERE"""
+)
+try:
+    print(
+        "Google Cloud Speech thinks you said "
+        + r.recognize_google_cloud(
+            audio, credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS
+        )
+    )
+except sr.UnknownValueError:
+    print("Google Cloud Speech could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Cloud Speech service; {0}".format(e))
