13d9bedda7ca79211278d4cefbcd5fc8ddfb1aff
Jade Fisher <jadefisher@Jades-MacBook-Air.local>
2023-04-27 19:12:43 -0600

    add game

--- src/mic.py
+++ src/mic.py
@@ -3,42 +3,15 @@ import speech_recognition as sr
 # obtain audio from the microphone
 r = sr.Recognizer()
 with sr.Microphone() as source:
+
+    response = {
+        "success": True,
+        "error": None,
+        "transcription": None
+    }
     print("Say something!")
     audio = r.listen(source)
+    response["transcription"] = r.recognize_google(audio, language='en-US')
 
-    # recognize speech using google speech recognition
-try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-    # instead of `r.recognize_google(audio)`
-    print(
-        "Google Speech Recognition thinks you said "
-        + r.recognize_google(
-            audio,
-            key="657215957578-ikc0765fs4geri7tj55pqgrgtbrndr7p.apps.googleusercontent.com",
-        )
-    )
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print(
-        "Could not request results from Google Speech Recognition service; {0}".format(
-            e
-        )
-    )
+    print(response)
 
-# recognize speech using Google Cloud Speech
-GOOGLE_CLOUD_SPEECH_CREDENTIALS = (
-    r"""INSERT THE CONTENTS OF THE GOOGLE CLOUD SPEECH JSON CREDENTIALS FILE HERE"""
-)
-try:
-    print(
-        "Google Cloud Speech thinks you said "
-        + r.recognize_google_cloud(
-            audio, credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS
-        )
-    )
-except sr.UnknownValueError:
-    print("Google Cloud Speech could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Cloud Speech service; {0}".format(e))
